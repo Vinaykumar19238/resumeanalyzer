7117e2861f0b4a7d0dbf373101e3f5ffa548c3d3
Vinaykumar19238 <vinaykodari83@gmail.com>
2026-03-30 12:29:58 +0530

    Add datasource URL logging for Render debugging

--- Backend/backend/src/main/java/com/project/resumeanalyzer/ResumeAnalyzerApplication.java
+++ Backend/backend/src/main/java/com/project/resumeanalyzer/ResumeAnalyzerApplication.java
@@ -1,13 +1,27 @@
 package com.project.resumeanalyzer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ResumeAnalyzerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ResumeAnalyzerApplication.class);
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ResumeAnalyzerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logDbUrl() {
+		logger.info("Connecting to Database: {}", dbUrl.contains("@") ? dbUrl.substring(dbUrl.lastIndexOf("@")) : dbUrl);
 	}
 
 }
